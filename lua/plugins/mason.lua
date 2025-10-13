d772e34275513c53bdab6f5c45ca62738d8060a3
edinhofjr <edinhofassbinder9876@gmail.com>
2025-10-13 14:18:00 -0300

    fix removido função de carregamento de binários de lsp do mason, motivo: plugin já realiza internamente

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,10 +1,4 @@
-local pm = require("utils.pathman")
-
-local LSP_MASON_FOLDER = vim.fn.stdpath "data" .. "/mason/bin"
 return {
     "mason-org/mason.nvim",
-    init = function (_)
-        pm.add(LSP_MASON_FOLDER)
-    end,
     opts = {},
 }

7ec4d441375185d9c463c49ff0b9ea57596759b5
edinhofjr <edinhofassbinder9876@gmail.com>
2025-10-13 14:16:18 -0300

    fix nome do plugin instalador do mason modificado para mason-installer

---
+++ lua/plugins/mason-installer.lua
@@ -0,0 +1,11 @@
+# Just to use "ensure_installed" for Mason Plugin
+return {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+        ensure_installed = {
+            "lua-language-server",
+            "gopls",
+            "docker-language-server"
+        }
+    }
+}

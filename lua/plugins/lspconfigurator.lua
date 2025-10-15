382dd7f603da0848095ff21652be1eadcec4d987
edinhofjr <edinhofassbinder9876@gmail.com>
2025-10-15 16:56:27 -0300

    added lspconfigurator config and gitignore changed to ignore plugin dev folder

---
+++ lua/plugins/lspconfigurator.lua
@@ -0,0 +1,10 @@
+return {
+    dir = vim.fn.stdpath "config" .. "/lua/lspconfigurator",
+    dependencies = {
+        "mason-org/mason.nvim"
+    },
+    ---@type opts
+    opts = {
+        ensure_installed = {"lua"}
+    }
+}

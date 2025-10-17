b285150cdacb1355db2c0b07d35296d0c82abaa1
edinhofjr <ediofassbinder9876@gmail.com>
2025-10-17 08:54:39 -0300

    added function to debug pwd call

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
+vim.api.nvim_create_autocmd({"DirChanged"}, {
+  callback = function()
+    print("CWD mudou para: " .. vim.loop.cwd())
+    print(debug.traceback())  -- mostra quem chamou
+  end
+})
+
 require("lazy-init")
 require("setup-init")
